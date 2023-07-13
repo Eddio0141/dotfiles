49e4a62ee12282b56f17b90e26f6fbe235741945
Eddio0141 <eddio0141@gmail.com>
2023-07-13 03:07:44 +0100

    updated

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.yuu.imports = [ ./home-manager.nix ];
+              home-manager.users.yuu.imports = [ ./home.nix ];
             }
           ];
         };
