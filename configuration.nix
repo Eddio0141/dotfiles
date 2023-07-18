31347d3bad8736592faac414a22e6ecb233f62f2
Eddio0141 <eddio0141@gmail.com>
2023-07-18 01:18:43 +0100

    UNITYHUB FINALLY WORKS

--- configuration.nix
+++ configuration.nix
@@ -304,7 +304,7 @@
 
   xdg.portal = {
     enable = true;
-    xdgOpenUsePortal = true;
+    # xdgOpenUsePortal = true;
     extraPortals = with pkgs; [
       # xdg-desktop-portal
       xdg-desktop-portal-gtk
