f089d5942b683e0145d423e000aef24b7d4e437c
Eddio0141 <eddio0141@gmail.com>
2023-10-22 17:53:58 +0100

    updated lock

--- configuration.nix
+++ configuration.nix
@@ -210,7 +210,7 @@
 
   # HIP
   systemd.tmpfiles.rules = [
-    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.hip}"
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
   ];
 
   hardware.opengl.extraPackages = with pkgs; [
