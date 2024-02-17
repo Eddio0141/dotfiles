21ca228683c8f4f36ab39b6bbaf48514e88263a1
Eddio0141 <eddio0141@gmail.com>
2024-02-17 08:17:04 +0000

    removed thunar

--- configuration.nix
+++ configuration.nix
@@ -384,16 +384,6 @@
     enable = true;
   };
 
-  # thunar stuff
-  # programs.thunar = {
-  #   enable = true;
-  #   plugins = with pkgs.xfce; [
-  #     thunar-volman
-  #     thunar-archive-plugin
-  #     thunar-media-tags-plugin
-  #   ];
-  # };
-
   services.gvfs.enable = true; # Mount, trash, and other functionalities
   services.tumbler.enable = true; # Thumbnail support for images
 
