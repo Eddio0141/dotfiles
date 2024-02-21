f40ff9b7b7a70e24c68974d037bb3271139945f4
Eddio0141 <eddio0141@gmail.com>
2024-02-21 19:33:02 +0000

    changed hyprland input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "my system flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,7 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
-      url = "github:hyprwm/Hyprland/v0.35.0";
+      url = "github:hyprwm/Hyprland";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         xdph.follows = "xdph";
