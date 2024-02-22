87358508993610a095de59660a9fa06e0daba979
Eddio0141 <eddio0141@gmail.com>
2024-02-22 19:41:19 +0000

    cleaner way of setting up home-manager

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -3,18 +3,12 @@
   imports = [
     ../../modules/casual.nix 
     ./hardware-configuration.nix
-    home-manager.nixosModules.home-manager {
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        extraSpecialArgs = { inherit username inputs system; };
-        users.${username} = {
-          imports = [
-            ../../modules/casual-home.nix
-            inputs.hyprland.homeManagerModules.default
-          ];
-        };
-      };
-    }
   ];
+
+  home-manager.users.${username} = {
+    imports = [
+      ../../modules/casual-home.nix
+      inputs.hyprland.homeManagerModules.default
+    ];
+  };
 }
--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,14 @@
+{ username, inputs, system, home-manager, ... }:
 {
   imports = [
     ./de
     ./programs
+    home-manager.nixosModules.home-manager {
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        extraSpecialArgs = { inherit username inputs system; };
+      };
+    }
   ];
 }
