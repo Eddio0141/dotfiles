1f07801cfefe687f17fee114b5d2e134809f5584
Eddio0141 <eddio0141@gmail.com>
2024-03-13 20:14:50 +0000

    format

--- pkgs/programs/nvim/config/noice.nix
+++ pkgs/programs/nvim/config/noice.nix
@@ -1,5 +1,16 @@
 {
   plugins.noice = {
     enable = true;
+    # TODO don't override the whole table, but extend to it
+    cmdline.format = {
+      cmdline = {pattern = "^:"; icon = ""; lang = "vim";};
+      search_down = {kind = "search"; pattern = "^/"; icon = " "; lang = "regex";};
+      search_up = {kind = "search"; pattern = "?%?"; icon = " "; lang = "regex";};
+      filter = {pattern = "^:%s*!"; icon = "$"; lang = "bash";};
+      lua = {pattern = "^:%s*lua%s+"; icon = ""; lang = "lua";};
+      help = {pattern = "^:%s*he?l?p?%s+"; icon = "";};
+      inc_rename = { pattern = "^:IncRename%s+"; icon = "󰑕"; };
+      input = {};
+    };
   };
 }
