50191b535daa4e14fb1305de8d8aa1fc41ebd0f1
Eddio0141 <eddio0141@gmail.com>
2024-04-15 10:21:27 +0100

    changed some sound config

--- hosts/yuu-upwork/default.nix
+++ hosts/yuu-upwork/default.nix
@@ -52,8 +52,13 @@
   # services.printing.enable = true;
 
   # Enable sound.
-  sound.enable = true;
   hardware.pulseaudio.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
