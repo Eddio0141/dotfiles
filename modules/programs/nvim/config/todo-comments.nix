a375f750935b13446d4e8f586ce37caf6e86c57f
Eddio0141 <eddio0141@gmail.com>
2024-04-20 17:49:18 +0100

    for jumping to next todo

--- modules/programs/nvim/config/todo-comments.nix
+++ modules/programs/nvim/config/todo-comments.nix
@@ -1,9 +1,32 @@
 {
-  plugins.todo-comments = {
-    enable = true;
-    keymaps = {
-      todoTrouble.key = "<leader>st";
-      todoTelescope.key = "<leader>sT";
+  config = {
+    keymaps = [
+      {
+        mode = "n";
+        key = "]t";
+        action = ''
+        require("todo-comments").jump_next()
+        '';
+        lua = true;
+        options.desc = "Jump to next todo";
+      }
+      {
+        mode = "n";
+        key = "[t";
+        action = ''
+        require("todo-comments").jump_prev()
+        '';
+        lua = true;
+        options.desc = "Jump to previous todo";
+      }
+    ];
+    
+    plugins.todo-comments = {
+      enable = true;
+      keymaps = {
+        todoTrouble.key = "<leader>st";
+        todoTelescope.key = "<leader>sT";
+      };
     };
   };
 }
