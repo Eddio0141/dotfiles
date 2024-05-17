d39a8a05a26995df5ced0684320779dc5c9a8d28
Eddio0141 <eddio0141@gmail.com>
2024-05-17 21:47:58 +0100

    added basic laptop config

--- hosts/yuu-laptop/default.nix
+++ hosts/yuu-laptop/default.nix
@@ -1,7 +1,16 @@
-{ inputs, username, ... }:
+{ username, inputs, system, home-manager, ... }:
 {
   imports = [
+    ../../modules/casual.nix
+    ./hardware-configuration.nix
   ];
+
+  home-manager.users.${username} = {
+    imports = [
+      ../../modules/casual-home.nix
+      inputs.hyprland.homeManagerModules.default
+    ];
+  };
 
   networking.hostName = "yuu-laptop";
 }
