2d9f9d534010b183a6e9f862f02645f52dd4af91
Eddio0141 <eddio0141@gmail.com>
2024-05-22 21:58:49 +0100

    swap to nil_ls for nix lsp, added formatting

--- modules/programs/nvim/config/lsp.nix
+++ modules/programs/nvim/config/lsp.nix
@@ -1,8 +1,12 @@
+{ pkgs, ... }:
 {
   plugins.lsp = {
     enable = true;
     servers = {
-      nixd.enable = true;
+      nil_ls = {
+        enable = true;
+        settings.formatting.command = [ "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt" ];
+      };
       jsonls.enable = true;
       ccls.enable = true;
       omnisharp.enable = true;
