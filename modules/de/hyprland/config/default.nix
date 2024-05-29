4c6369d2e4535b6f6327d33e6b8e8600735cf106
Eddio0141 <eddio0141@gmail.com>
2024-05-29 13:51:05 +0100

    added scroll to workspace bind

--- modules/de/hyprland/config/default.nix
+++ modules/de/hyprland/config/default.nix
@@ -1,4 +1,8 @@
 { pkgs, cfg, lib }:
+let
+  mainMod = "SUPER";
+  altCombo = "${mainMod} ALT";
+in
 {
   # "$border_pink_active" = "rgba(ff8cecff)";
   # "$border_pink_inactive" = "rgba(595959aa)";
@@ -149,84 +153,89 @@
     };
   };
 
-  "$mainMod" = "SUPER";
-  "$altCombo" = "SUPER ALT";
-
   bind = [
-    "$mainMod, Q, exec, kitty"
-    "$mainMod, W, killactive,"
+    # general binds
+    "${mainMod}, Q, exec, kitty"
+    "${mainMod}, W, killactive,"
+    "${mainMod}, E, exec, dolphin"
+    "${mainMod}, S, exec, pidof wofi || wofi --show drun -I -m -i -W 25% -H 75%"
+
     "CTRL ALT, Delete, exec, pidof wlogout || wlogout"
     "CTRL ALT, BackSpace, exec, hyprlock"
-    "$mainMod, E, exec, dolphin"
-    "$mainMod, T, togglefloating, "
-    "$mainMod, S, exec, pidof wofi || wofi --show drun -I -m -i -W 25% -H 75%"
-    "$mainMod, P, pseudo, # dwindle"
-    "$mainMod, U, togglesplit, # dwindle"
-    "$mainMod, F, fullscreen,"
-    "$mainMod, G, togglegroup,"
-    "$mainMod, End, lockactivegroup, toggle"
-    "$mainMod, Next, changegroupactive, f"
-    "$mainMod, Prior, changegroupactive, b"
-    "$mainMod SHIFT, End, moveoutofgroup,"
+
+    "${mainMod}, T, togglefloating, "
+    "${mainMod}, P, pseudo,"
+    "${mainMod}, U, togglesplit,"
+    "${mainMod}, F, fullscreen,"
+    "${mainMod}, G, togglegroup,"
+
+    "${mainMod}, End, lockactivegroup, toggle"
+    "${mainMod}, Next, changegroupactive, f"
+    "${mainMod}, Prior, changegroupactive, b"
+    "${mainMod} SHIFT, End, moveoutofgroup,"
 
     # Move focus with mainMod + arrow keys
-    "$mainMod, left, movefocus, l"
-    "$mainMod, right, movefocus, r"
-    "$mainMod, up, movefocus, u"
-    "$mainMod, down, movefocus, d"
-    "$mainMod, h, movefocus, l"
-    "$mainMod, l, movefocus, r"
-    "$mainMod, k, movefocus, u"
-    "$mainMod, j, movefocus, d"
+    "${mainMod}, left, movefocus, l"
+    "${mainMod}, right, movefocus, r"
+    "${mainMod}, up, movefocus, u"
+    "${mainMod}, down, movefocus, d"
+    "${mainMod}, h, movefocus, l"
+    "${mainMod}, l, movefocus, r"
+    "${mainMod}, k, movefocus, u"
+    "${mainMod}, j, movefocus, d"
 
     # Move windows with mainMod + SHIFT + arrow keys
-    "$mainMod SHIFT, left, movewindow, l"
-    "$mainMod SHIFT, right, movewindow, r"
-    "$mainMod SHIFT, up, movewindow, u"
-    "$mainMod SHIFT, down, movewindow, d"
-    "$mainMod SHIFT, h, movewindow, l"
-    "$mainMod SHIFT, l, movewindow, r"
-    "$mainMod SHIFT, k, movewindow, u"
-    "$mainMod SHIFT, j, movewindow, d"
+    "${mainMod} SHIFT, left, movewindow, l"
+    "${mainMod} SHIFT, right, movewindow, r"
+    "${mainMod} SHIFT, up, movewindow, u"
+    "${mainMod} SHIFT, down, movewindow, d"
+    "${mainMod} SHIFT, h, movewindow, l"
+    "${mainMod} SHIFT, l, movewindow, r"
+    "${mainMod} SHIFT, k, movewindow, u"
+    "${mainMod} SHIFT, j, movewindow, d"
 
     # Switch workspaces with mainMod + [0-9]
-    "$mainMod, 1, workspace, 1"
-    "$mainMod, 2, workspace, 2"
-    "$mainMod, 3, workspace, 3"
-    "$mainMod, 4, workspace, 4"
-    "$mainMod, 5, workspace, 5"
-    "$mainMod, 6, workspace, 6"
-    "$mainMod, 7, workspace, 7"
-    "$mainMod, 8, workspace, 8"
-    "$mainMod, 9, workspace, 9"
-    "$mainMod, 0, workspace, 10"
+    "${mainMod}, 1, workspace, 1"
+    "${mainMod}, 2, workspace, 2"
+    "${mainMod}, 3, workspace, 3"
+    "${mainMod}, 4, workspace, 4"
+    "${mainMod}, 5, workspace, 5"
+    "${mainMod}, 6, workspace, 6"
+    "${mainMod}, 7, workspace, 7"
+    "${mainMod}, 8, workspace, 8"
+    "${mainMod}, 9, workspace, 9"
+    "${mainMod}, 0, workspace, 10"
+
+    # Switch workspaces
+    "${mainMod} CTRL, h, workspace, e-1"
+    "${mainMod} CTRL, l, workspace, e+1"
 
     # Move active window to a workspace with mainMod + SHIFT + [0-9]
-    "$mainMod SHIFT, 1, movetoworkspace, 1"
-    "$mainMod SHIFT, 2, movetoworkspace, 2"
-    "$mainMod SHIFT, 3, movetoworkspace, 3"
-    "$mainMod SHIFT, 4, movetoworkspace, 4"
-    "$mainMod SHIFT, 5, movetoworkspace, 5"
-    "$mainMod SHIFT, 6, movetoworkspace, 6"
-    "$mainMod SHIFT, 7, movetoworkspace, 7"
-    "$mainMod SHIFT, 8, movetoworkspace, 8"
-    "$mainMod SHIFT, 9, movetoworkspace, 9"
-    "$mainMod SHIFT, 0, movetoworkspace, 10"
+    "${mainMod} SHIFT, 1, movetoworkspace, 1"
+    "${mainMod} SHIFT, 2, movetoworkspace, 2"
+    "${mainMod} SHIFT, 3, movetoworkspace, 3"
+    "${mainMod} SHIFT, 4, movetoworkspace, 4"
+    "${mainMod} SHIFT, 5, movetoworkspace, 5"
+    "${mainMod} SHIFT, 6, movetoworkspace, 6"
+    "${mainMod} SHIFT, 7, movetoworkspace, 7"
+    "${mainMod} SHIFT, 8, movetoworkspace, 8"
+    "${mainMod} SHIFT, 9, movetoworkspace, 9"
+    "${mainMod} SHIFT, 0, movetoworkspace, 10"
 
     # Switch to empty workspace
-    "$mainMod SHIFT, E, workspace, empty"
+    "${mainMod} SHIFT, E, workspace, empty"
 
     # Special workspace actions
-    "$mainMod SHIFT, S, movetoworkspacesilent, special"
-    "$mainMod CTRL, S, togglespecialworkspace"
+    "${mainMod} SHIFT, S, movetoworkspacesilent, special"
+    "${mainMod} CTRL, S, togglespecialworkspace"
 
     # Scroll through existing workspaces with mainMod + scroll
-    "$mainMod, mouse_up, workspace, e+1"
-    "$mainMod, mouse_down, workspace, e-1"
+    "${mainMod}, mouse_up, workspace, e+1"
+    "${mainMod}, mouse_down, workspace, e-1"
 
     # Switch monitors
-    "$mainMod, comma, focusmonitor, 0"
-    "$mainMod, period, focusmonitor, 1"
+    "${mainMod}, comma, focusmonitor, 0"
+    "${mainMod}, period, focusmonitor, 1"
 
     # screenshot
     "CTRL, Print, exec, grimblast --notify --freeze copysave area"
@@ -237,9 +246,9 @@
     ", XF86AudioMute, exec, pamixer --toggle-mute"
 
     # music
-    "$altCombo, P, exec, clementine --play-pause"
-    "$altCombo, O, exec, clementine --next"
-    "$altCombo, I, exec, clementine --previous"
+    "${altCombo}, P, exec, clementine --play-pause"
+    "${altCombo}, O, exec, clementine --next"
+    "${altCombo}, I, exec, clementine --previous"
     ", XF86AudioPlay, exec, clementine --play-pause"
     ", XF86AudioNext, exec, clementine --next"
     ", XF86AudioPrev, exec, clementine --previous"
@@ -252,7 +261,7 @@
 
   bindm = [
     # Move/resize windows with mainMod + LMB/RMB and dragging
-    "$mainMod, mouse:272, movewindow"
-    "$mainMod, mouse:273, resizewindow"
+    "${mainMod}, mouse:272, movewindow"
+    "${mainMod}, mouse:273, resizewindow"
   ];
 }
