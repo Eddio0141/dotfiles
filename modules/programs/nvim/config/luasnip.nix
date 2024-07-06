12ca49a35322a0e458b54aa1a24ae8d5f78ec2bc
Eddio0141 <eddio0141@gmail.com>
2024-07-06 19:57:09 +0100

    nvim: added tab and s-tab for snippet navigation

--- modules/programs/nvim/config/luasnip.nix
+++ modules/programs/nvim/config/luasnip.nix
@@ -1,8 +1,37 @@
 {
-  plugins.luasnip = {
-    enable = true;
-    extraConfig = {
-      store_selection_keys = "<Tab>";
-    };
+  config = {
+    plugins.luasnip.enable = true;
+
+    keymaps = [
+      {
+        key = "<tab>";
+        mode = "i";
+        action = ''
+          function () 
+            return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+          end
+        '';
+        lua = true;
+        options = {
+          desc = "Jump or expand snippet";
+          expr = true;
+          silent = true;
+        };
+      }
+      {
+        key = "<tab>";
+        mode = "s";
+        action = ''function() require("luasnip").jump(1) end'';
+        lua = true;
+        options.desc = "Jump snippet forwards";
+      }
+      {
+        key = "<s-tab>";
+        mode = [ "i" "s" ];
+        action = ''function () require("luasnip").jump(-1) end'';
+        lua = true;
+        options.desc = "Jump snippet backwards";
+      }
+    ];
   };
 }
