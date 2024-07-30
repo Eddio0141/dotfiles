b7853970b8b1363ba3995c91e5e18fa8eba78b2d
Eddio0141 <eddio0141@gmail.com>
2024-07-30 18:07:08 +0100

    don't escape in terminal mode

--- modules/programs/nvim/config/better-escape.nix
+++ modules/programs/nvim/config/better-escape.nix
@@ -1,5 +1,13 @@
 {
   plugins.better-escape = {
     enable = true;
+    settings.mappings = {
+      t = {
+        j = {
+          j = false;
+          k = false;
+        };
+      };
+    };
   };
 }
