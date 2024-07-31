bff84828d2a55093c3a5cb4a826404aaa2e29769
Eddio0141 <eddio0141@gmail.com>
2024-07-31 13:37:18 +0100

    removed overlays not required

--- modules/programs/gpu-screen-recorder/default.nix
+++ modules/programs/gpu-screen-recorder/default.nix
@@ -19,32 +19,6 @@ in
 
   config = mkMerge [
     (mkIf cfg.enable {
-      # overlays
-      nixpkgs.overlays = [
-        (self: super: {
-          gpu-screen-recorder = super.gpu-screen-recorder.overrideAttrs (old: {
-            postInstall = ''
-              install -Dt $out/bin gpu-screen-recorder gsr-kms-server
-              mkdir $out/bin/.wrapped
-              mv $out/bin/gpu-screen-recorder $out/bin/.wrapped/
-              makeWrapper "$out/bin/.wrapped/gpu-screen-recorder" "$out/bin/gpu-screen-recorder" \
-              --prefix LD_LIBRARY_PATH : ${self.libglvnd}/lib \
-              --suffix PATH : $out/bin
-            ''; # prefix -> suffix
-          });
-          gpu-screen-recorder-gtk = super.gpu-screen-recorder-gtk.overrideAttrs (old: {
-            installPhase = ''
-              install -Dt $out/bin/ gpu-screen-recorder-gtk
-              install -Dt $out/share/applications/ gpu-screen-recorder-gtk.desktop
-              gappsWrapperArgs+=(--prefix PATH : /run/wrappers/bin:${super.lib.makeBinPath [ self.gpu-screen-recorder ]})
-              # gappsWrapperArgs+=(--prefix PATH : /run/wrappers/bin)
-              # we also append /run/opengl-driver/lib as it otherwise fails to find libcuda.
-              gappsWrapperArgs+=(--prefix LD_LIBRARY_PATH : ${super.lib.makeLibraryPath [ self.libglvnd ]}:/run/opengl-driver/lib)
-            ''; # add /run/wrappers/bin to prefix
-          });
-        })
-      ];
-
       environment.systemPackages = with pkgs; [
         gpu-screen-recorder-gtk
       ];
