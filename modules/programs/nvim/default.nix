583aa562d2794bddbbd13fbd6b6de6d509da4b28
Eddio0141 <eddio0141@gmail.com>
2024-07-31 13:37:32 +0100

    added overlays for fixing omnisharp

--- modules/programs/nvim/default.nix
+++ modules/programs/nvim/default.nix
@@ -1,5 +1,4 @@
 { config, lib, pkgs, inputs, system, ... }:
-with lib;
 let
   cfg = config.yuu.programs.nvim;
   nixvim = inputs.nixvim.legacyPackages.${system};
@@ -10,18 +9,33 @@ let
   };
 in
 {
-  options.yuu.programs.nvim.enable = mkEnableOption "nvim";
+  options.yuu.programs.nvim.enable = lib.mkEnableOption "nvim";
 
-  config = (mkIf cfg.enable {
-    # TODO i swear there were hm module that enalbes those
+  config = (lib.mkIf cfg.enable {
+    # TODO: i swear there were hm module that enables those
     environment.sessionVariables = {
       EDITOR = "nvim";
       VISUAL = "nvim";
       GIT_EDITOR = "nvim";
     };
-    # TODO also hm for this too? idk
+    # TODO: also hm for this too? idk
     environment.systemPackages = [
       (nixvim.makeNixvimWithModule nixvimModule)
+    ];
+
+    nixpkgs.overlays = [
+      # NOTE: check flake.nix
+      (final: prev: {
+        omnisharp-roslyn = prev.omnisharp-roslyn.overrideAttrs {
+          version = "1.39.11-fix";
+          src = prev.fetchFromGitHub {
+            owner = "Eddio0141";
+            repo = "omnisharp-roslyn";
+            rev = "fix-nvim-lsp";
+            hash = "sha256-FyWHdug2NZGGcn7/eiTi0RklxdC+rtEb51+/l8lebk4=";
+          };
+        };
+      })
     ];
   });
 }
