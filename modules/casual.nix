bb9f6ea90a2ac5a0f3746df580265690d66eade6
Eddio0141 <eddio0141@gmail.com>
2024-09-08 18:29:23 +0100

    fcitx5 fix

--- modules/casual.nix
+++ modules/casual.nix
@@ -92,11 +92,14 @@ in
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-      libsForQt5.fcitx5-qt
-    ];
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+        libsForQt5.fcitx5-qt
+      ];
+      waylandFrontend = true;
+    };
   };
 
   services = {
