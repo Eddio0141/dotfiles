4f17bea0c16abc0d05b1b50643519dddcd2d0ae9
Eddio0141 <eddio0141@gmail.com>
2024-10-26 18:27:23 +0100

    don't autoswitch to headset profile on bluetooth

--- modules/casual.nix
+++ modules/casual.nix
@@ -150,6 +150,12 @@ in
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+
+    wireplumber.extraConfig."11-bluetooth-policy" = {
+      "wireplumber.settings" = {
+        "bluetooth.autoswitch-to-headset-profile" = false;
+      };
+    };
   };
 
   programs = {
