f97ed9743359c4513117931315ff3a01c80b194b
Eddio0141 <eddio0141@gmail.com>
2024-11-10 20:55:25 +0000

    format file

--- modules/pack/comfy-shell/default.nix
+++ modules/pack/comfy-shell/default.nix
@@ -1,4 +1,10 @@
-{ config, lib, pkgs, username, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  username,
+  ...
+}:
 with lib;
 let
   cfg = config.yuu.pack.comfy-shell;
@@ -6,91 +12,93 @@ in
 {
   options.yuu.pack.comfy-shell.enable = mkEnableOption "shell environment";
 
-  config = (mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      tldr
-    ];
+  config = (
+    mkIf cfg.enable {
+      environment.systemPackages = with pkgs; [
+        tldr
+      ];
 
-    # zsh setups
-    users.users.${username}.shell = pkgs.zsh;
+      # zsh setups
+      users.users.${username}.shell = pkgs.zsh;
 
-    programs = {
-      zsh.enable = true;
-      nix-index.enable = true;
-      nh = {
-        enable = true;
-        flake = "/home/${username}/dotfiles";
-        clean = {
-          enable = true;
-          dates = "weekly";
-          extraArgs = "--keep-since 31d";
-        };
-      };
-    };
-
-    users.defaultUserShell = pkgs.zsh;
-
-    environment.shells = [ pkgs.zsh ];
-
-    home-manager.users.${username} = {
       programs = {
-        nix-index = {
+        zsh.enable = true;
+        nix-index.enable = true;
+        nh = {
           enable = true;
-          enableZshIntegration = true;
-        };
-
-        # direnv
-        direnv = {
-          enable = true;
-          enableZshIntegration = true;
-          nix-direnv.enable = true;
-        };
-
-        # zsh
-        zsh = {
-          enable = true;
-          autosuggestion.enable = true;
-          enableCompletion = true;
-          syntaxHighlighting.enable = true;
-
-          shellAliases = {
-            update = "nh os switch";
-            update-test = "nh os test";
-            upgrade = "nh os switch -u";
-          };
-
-          # oh my zsh
-          oh-my-zsh = {
+          flake = "/home/${username}/dotfiles";
+          clean = {
             enable = true;
-            plugins = [ "git" ];
-            theme = "fino-time";
+            dates = "weekly";
+            extraArgs = "--keep-since 31d";
+          };
+        };
+      };
+
+      users.defaultUserShell = pkgs.zsh;
+
+      environment.shells = [ pkgs.zsh ];
+
+      home-manager.users.${username} = {
+        programs = {
+          nix-index = {
+            enable = true;
+            enableZshIntegration = true;
           };
 
-          initExtra = ''
-            function zvm_after_init() {
-              bindkey '^ ' autosuggest-accept
-            }
-            
-            if [[ $- = *i* ]]; then
-            fastfetch
+          # direnv
+          direnv = {
+            enable = true;
+            enableZshIntegration = true;
+            nix-direnv.enable = true;
+          };
 
-            echo "Welcome back $USER!"
-            fi
-          '';
+          # zsh
+          zsh = {
+            enable = true;
+            autosuggestion.enable = true;
+            enableCompletion = true;
+            syntaxHighlighting.enable = true;
 
-          plugins = [
-            {
-              name = "vi-mode";
-              src = pkgs.zsh-vi-mode;
-              file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
-            }
-          ];
+            shellAliases = {
+              update = "nh os switch";
+              update-test = "nh os test";
+              upgrade = "nh os switch -u";
+            };
+
+            # oh my zsh
+            oh-my-zsh = {
+              enable = true;
+              plugins = [ "git" ];
+              theme = "fino-time";
+            };
+
+            initExtra = ''
+              function zvm_after_init() {
+                bindkey '^ ' autosuggest-accept
+              }
+
+              if [[ $- = *i* ]]; then
+              fastfetch
+
+              echo "Welcome back $USER!"
+              fi
+            '';
+
+            plugins = [
+              {
+                name = "vi-mode";
+                src = pkgs.zsh-vi-mode;
+                file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+              }
+            ];
+          };
+
+          fd.enable = true;
         };
-
-        fd.enable = true;
       };
-    };
 
-    yuu.programs.fastfetch.enable = true;
-  });
+      yuu.programs.fastfetch.enable = true;
+    }
+  );
 }
