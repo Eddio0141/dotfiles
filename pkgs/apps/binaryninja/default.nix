7245dee7152bc7b3dcb5dd495e1de122017baa6b
Eddio0141 <eddio0141@gmail.com>
2024-11-10 17:15:57 +0000

    jank binary ninja change

--- pkgs/apps/binaryninja/default.nix
+++ pkgs/apps/binaryninja/default.nix
@@ -1,25 +1,53 @@
-{ stdenv
-, fetchzip
-, lib
-, makeWrapper
-, free ? true
-, ...
+{
+  stdenv,
+  fetchzip,
+  lib,
+  makeWrapper,
+  autoPatchelfHook,
+  unzip,
+  wayland,
+  libGL,
+  qt6,
+  python310,
+  glib,
+  fontconfig,
+  dbus,
+  free ? true,
+  ...
 }:
 stdenv.mkDerivation {
   pname = "binaryninja";
   version = if free then "4.1.5747-stable-free" else throw "TODO";
   src =
     if free then
-      fetchzip
-        {
-          url = "https://cdn.binary.ninja/installers/binaryninja_free_linux.zip";
-          hash = "sha256-ItzkG2t+wTRpHd2wXxZekyBJqS12D9MAzY6/U38tEfI=";
-        }
+      fetchzip {
+        url = "https://cdn.binary.ninja/installers/binaryninja_free_linux.zip";
+        hash = "sha256-iTTa5da8rsR1E50HQMqSidAcjT5fq4z6jp3eyg3XEN0=";
+      }
     else
       throw "TODO";
 
-  buildInputs = [ makeWrapper ];
+  buildInputs = [
+    autoPatchelfHook
+    makeWrapper
+    unzip
+    wayland
+    libGL
+    qt6.full
+    qt6.qtbase
+    python310
+    stdenv.cc.cc.lib
+    glib
+    fontconfig
+    dbus
+  ];
+  nativeBuildInputs = [
+    qt6.wrapQtAppsHook
+    python310.pkgs.wrapPython
+  ];
 
+  dontWrapQtApps = true;
+  buildPhase = ":";
   installPhase = ''
     mkdir -p $out/bin
     mkdir -p $out/opt
@@ -27,6 +55,11 @@ stdenv.mkDerivation {
     makeWrapper $out/opt/binaryninja \
       $out/bin/binaryninja \
       --prefix "QT_QPA_PLATFORM" ":" "wayland"
+  '';
+
+  postFixup = ''
+    patchelf --debug --add-needed libpython3.so \
+      "$out/opt/binaryninja"
   '';
 
   meta = with lib; {
