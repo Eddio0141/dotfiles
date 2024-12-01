ba48621139865c87fccc46429aff5d3caa318bed
Eddio0141 <eddio0141@gmail.com>
2024-12-01 16:03:21 +0000

    reverted bibata cursor to 2.0.6 to fix offset

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
   # NOTE: omnisharp causes shit to go down, for now added insecure package "dotnet-core-combined"
   # NOTE: also same with dotnet-sdk-6.0.428, dotnet-sdk-wrapped-6.0.428, dotnet-runtime-wrapped-6.0.36, dotnet-runtime-6.0.36, dotnet-sdk-7.0.410, dotnet-sdk-wrapped-7.0.410
   # TODO: retheme yazi, fix colours in help menu hover
+  # TODO: stylix-nixos.nix, remove overrideAttrs once this is resolved https://github.com/ful1e5/Bibata_Cursor/issues/173
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
--- modules/stylix-nixos.nix
+++ modules/stylix-nixos.nix
@@ -5,7 +5,34 @@
     image = ../assets/wallpaper/frieren.png;
     polarity = "dark";
     cursor = {
-      package = pkgs.bibata-cursors;
+      package = (
+        pkgs.bibata-cursors.overrideAttrs rec {
+          version = "2.0.6";
+          src = pkgs.fetchFromGitHub {
+            owner = "ful1e5";
+            repo = "Bibata_Cursor";
+            rev = "v${version}";
+            hash = "sha256-iLBgQ0reg8HzUQMUcZboMYJxqpKXks5vJVZMHirK48k=";
+          };
+          bitmaps = pkgs.fetchzip {
+            url = "https://github.com/ful1e5/Bibata_Cursor/releases/download/v${version}/bitmaps.zip";
+            hash = "sha256-8ujkyqby5sPcnscIPkay1gvd/1CH4R9yMJs1nH/mx8M=";
+          };
+          buildPhase = ''
+            runHook preBuild
+
+            ctgen build.toml -p x11 -d $bitmaps/Bibata-Modern-Amber -n 'Bibata-Modern-Amber' -c 'Yellowish and rounded edge bibata cursors.'
+            ctgen build.toml -p x11 -d $bitmaps/Bibata-Modern-Classic -n 'Bibata-Modern-Classic' -c 'Black and rounded edge Bibata cursors.'
+            ctgen build.toml -p x11 -d $bitmaps/Bibata-Modern-Ice -n 'Bibata-Modern-Ice' -c 'White and rounded edge Bibata cursors.'
+
+            ctgen build.toml -p x11 -d $bitmaps/Bibata-Original-Amber -n 'Bibata-Original-Amber' -c 'Yellowish and sharp edge Bibata cursors.'
+            ctgen build.toml -p x11 -d $bitmaps/Bibata-Original-Classic -n 'Bibata-Original-Classic' -c 'Black and sharp edge Bibata cursors.'
+            ctgen build.toml -p x11 -d $bitmaps/Bibata-Original-Ice -n 'Bibata-Original-Ice' -c 'White and sharp edge Bibata cursors.'
+
+            runHook postBuild
+          '';
+        }
+      );
       name = "Bibata-Modern-Amber";
       size = 24;
     };
