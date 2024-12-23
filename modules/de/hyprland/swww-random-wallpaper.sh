8a7b53a32d1b1e6d2edf22b4b9998c655a836e5a
Eddio0141 <eddio0141@gmail.com>
2024-12-23 18:33:52 +0000

    try new images if error loading

--- modules/de/hyprland/swww-random-wallpaper.sh
+++ modules/de/hyprland/swww-random-wallpaper.sh
@@ -40,7 +40,7 @@ INTERVAL=300
 #    -   no:   Do not resize the image
 #    -   crop: Resize the image to fill the whole screen, cropping out parts that don't fit
 #    -   fit:  Resize the image to fit inside the screen, preserving the original aspect ratio
-RESIZE_TYPE="fit"
+RESIZE_TYPE="crop"
 
 DISPLAY_LIST=$(swww query | grep -Po "^[^:]+")
 
@@ -54,17 +54,23 @@ while true; do
       | while read -r img; do
           # Set a different image for each display
           for disp in $DISPLAY_LIST; do
-              # if there is no image try to get one
-              if [ "X" = "X${img}" ]; then
-                  if read -r img; then
-                      true
-                  else # if there are no more images, refresh the list
-                      break 2
+              while true; do
+                  # if there is no image try to get one
+                  if [ "X" = "X${img}" ]; then
+                      if read -r img; then
+                          true
+                      else # if there are no more images, refresh the list
+                          break 3
+                      fi
                   fi
-              fi
-              swww img --resize=$RESIZE_TYPE --outputs "$disp" "$img"
-              # make sure each image is only used once
-              img=""
+                  if swww img --resize=$RESIZE_TYPE --outputs "$disp" "$img"; then
+                      # make sure each image is only used once
+                      img=""
+                      break
+                  fi
+
+                  img=""
+              done
           done
           sleep $INTERVAL
       done
