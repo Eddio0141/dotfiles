57a84878266576af2bcf600a141c926d20035d2e
Eddio0141 <eddio0141@gmail.com>
2025-02-03 06:23:42 +0000

    removed dumb thing

--- modules/casual.nix
+++ modules/casual.nix
@@ -396,12 +396,10 @@ in
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
         "https://colmena.cachix.org"
-        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
