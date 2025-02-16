16761e2291fe2899e332cd0a51d43f6ff494dcbc
Eddio0141 <eddio0141@gmail.com>
2025-02-16 19:10:20 +0000

    added access-tokens to avoid timing out

--- modules/casual.nix
+++ modules/casual.nix
@@ -406,6 +406,8 @@ in
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
       ];
+      # TODO: hide if public
+      access-tokens = "github.com=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";
     };
   };
 
