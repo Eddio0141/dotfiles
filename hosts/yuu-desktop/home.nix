714a9092861769a3f8b070eac4f3ecd34c7fea54
Eddio0141 <eddio0141@gmail.com>
2025-02-16 21:51:31 +0000

    fixed broken service

--- hosts/yuu-desktop/home.nix
+++ hosts/yuu-desktop/home.nix
@@ -2,17 +2,14 @@
 {
   systemd.user.services.noisetorch =
     let
-      deviceUnit = ''sys-devices-pci0000:00-0000:00:08.1-0000:0a:00.3-usb3-3\x2d4-3\x2d4:1.0-sound-card4-controlC4.device'';
       deviceId = ''alsa_input.usb-Razer_Inc_Razer_Seiren_Mini_UC2220L03401755-00.mono-fallback'';
     in
     {
       Unit = {
         Description = "Noisetorch Noise Cancelling";
-        Requires = deviceUnit;
-        After = [
-          deviceUnit
-          "pipewire.service"
-        ];
+        Requires = "pipewire.socket";
+        StartLimitIntervalSec = 35;
+        StartLimitBurst = 9;
       };
 
       Service = {
@@ -25,7 +22,7 @@
       };
 
       Install = {
-        WantedBy = "default.target";
+        WantedBy = [ "default.target" ];
       };
     };
 }
