e8670be696abcf974161f62edfa5965fe018b844
Eddio0141 <eddio0141@gmail.com>
2025-04-10 21:40:08 +0100

    change wlogout hibernation to sleep then hibernation

--- modules/de/niri/default.nix
+++ modules/de/niri/default.nix
@@ -64,7 +64,47 @@ in
         niri.config = null;
         hyprlock.enable = true;
 
-        wlogout.enable = true;
+        wlogout = {
+          enable = true;
+          layout = [
+            {
+              label = "lock";
+              action = "loginctl lock-session";
+              text = "Lock";
+              keybind = "l";
+            }
+            {
+              label = "hibernate";
+              action = "systemctl suspend-then-hibernate";
+              text = "Hibernate";
+              keybind = "h";
+            }
+            {
+              label = "logout";
+              action = "loginctl terminate-user $USER";
+              text = "Logout";
+              keybind = "e";
+            }
+            {
+              label = "shutdown";
+              action = "systemctl poweroff";
+              text = "Shutdown";
+              keybind = "s";
+            }
+            {
+              label = "suspend";
+              action = "systemctl suspend";
+              text = "Suspend";
+              keybind = "u";
+            }
+            {
+              label = "reboot";
+              action = "systemctl reboot";
+              text = "Reboot";
+              keybind = "r";
+            }
+          ];
+        };
         waybar.settings.mainBar = {
           modules-left = [ "niri/workspaces" ];
           modules-center = [ "niri/window" ];
