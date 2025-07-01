65b773e086f21c7b6e27c33ae92ac9b85fe28375
Eddio0141 <eddio0141@gmail.com>
2025-07-01 23:18:20 +0100

    format file

--- modules/security/polkit-gnome/default.nix
+++ modules/security/polkit-gnome/default.nix
@@ -1,4 +1,13 @@
-{ config, lib, pkgs, inputs, system, home-manager, username, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  system,
+  home-manager,
+  username,
+  ...
+}:
 with lib;
 let
   cfg = config.yuu.security.polkit-gnome;
@@ -6,20 +15,22 @@ in
 {
   options.yuu.security.polkit-gnome.enable = mkEnableOption "polkit-gnome";
 
-  config = (mkIf cfg.enable {
-    # polkit agent
-    systemd.user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
+  config = (
+    mkIf cfg.enable {
+      # polkit agent
+      systemd.user.services.polkit-gnome-authentication-agent-1 = {
+        description = "polkit-gnome-authentication-agent-1";
+        wantedBy = [ "graphical-session.target" ];
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
       };
-    };
-  });
+    }
+  );
 }
