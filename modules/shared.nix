45cf7343d95f7e5f8221b5b89077ba71336f4b64
edcope <ed.cope@codethink.co.uk>
2025-08-15 11:21:03 +0100

    greetd cleanup

--- modules/shared.nix
+++ modules/shared.nix
@@ -179,7 +179,7 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time";
+          command = "${pkgs.tuigreet}/bin/tuigreet --time";
           user = "greeter";
         };
       };
